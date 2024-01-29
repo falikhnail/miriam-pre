1f1dd8c55c9c0f7cd74a51a1765cb600ca7845cd
Olivia-Sastro <im.lyxng@gmail.com>
2024-01-29 12:00:00 +0700

    fixing cetak laporan

--- resources/views/presensi/cetaklaporan.blade.php
+++ resources/views/presensi/cetaklaporan.blade.php
@@ -70,15 +70,15 @@
         <table style="width: 100%">
             <tr>
                 <td style="width: 30px">
-                    <img src="{{ asset('assets/img/logopresensi.png') }}" width="70" height="70" alt="">
+                    <img src="{{ asset('assets/img/logopresensimiriam.png') }}" width="70" height="70" alt="">
                 </td>
                 <td>
                     <span id="title">
                         LAPORAN PRESENSI KARYAWAN<br>
                         PERIODE {{ strtoupper($namabulan[$bulan]) }} {{ $tahun }}<br>
-                        PT. ADAM ADIFA<br>
+                        Rumah Sakit Ibu dan Anak Miriam<br>
                     </span>
-                    <span><i>Jln. H. Dahlan No. 75, Kecamatan Sindangrasa, Kabupaten Ciamis</i></span>
+                    <span><i>Jln. Ahmad Yani. No. 58, Kecamatan Kota, Kabupaten Kudus</i></span>
                 </td>
             </tr>
         </table>
@@ -193,15 +193,15 @@
 
         <table width="100%" style="margin-top:100px">
             <tr>
-                <td colspan="2" style="text-align: right">Tasikmalaya, {{ date('d-m-Y') }}</td>
+                <td colspan="2" style="text-align: right">Kudus, {{ date('d-m-Y') }}</td>
             </tr>
             <tr>
-                <td style="text-align: center; vertical-align:bottom" height="100px">
-                    <u>Qiana Aqila</u><br>
-                    <i><b>HRD Manager</b></i>
+                <td style="text-align: center; vertical-align:bottom" height="120px">
+                    <u>Arief Ersan</u><br>
+                    <i><b>HRD</b></i>
                 </td>
                 <td style="text-align: center; vertical-align:bottom">
-                    <u>Daffa</u><br>
+                    <u>dr. Billie Sancho Thea</u><br>
                     <i><b>Direktur</b></i>
                 </td>
             </tr>
