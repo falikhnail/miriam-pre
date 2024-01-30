288b322f7f7046f7f76bea1c57dad4823f140280
Falikh Rifqi Nail <68749934+falikhnail@users.noreply.github.com>
2024-01-30 14:19:17 +0700

    update login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -101,21 +101,6 @@
         }
     </script>
 
-    <!-- ///////////// Js Files ////////////////////  -->
-    <!-- Jquery -->
-    <script src="{{ asset('assets/js/lib/jquery-3.4.1.min.js') }}"></script>
-    <!-- Bootstrap-->
-    <script src="{{ asset('assets/js/lib/popper.min.js') }}"></script>
-    <script src="{{ asset('assets/js/lib/bootstrap.min.js') }}"></script>
-    <!-- Ionicons -->
-    <script type="module" src="https://unpkg.com/ionicons@5.0.0/dist/ionicons/ionicons.js"></script>
-    <!-- Owl Carousel -->
-    <script src="{{ asset('assets/js/plugins/owl-carousel/owl.carousel.min.js') }}"></script>
-    <!-- jQuery Circle Progress -->
-    <script src="{{ asset('assets/js/plugins/jquery-circle-progress/circle-progress.min.js') }}"></script>
-    <!-- Base Js File -->
-    <script src="{{ asset('assets/js/base.js') }}"></script>
-
     <script>
         $(function() {
             $("#show_hide_password").click(function(e) {
