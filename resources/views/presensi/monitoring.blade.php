0ae57034821239eefb37d559d9d8e6f81abb16eb
Falikh Rifqi Nail <68749934+falikhnail@users.noreply.github.com>
2024-03-09 08:48:02 +0700

    update accses for admin departement

--- resources/views/presensi/monitoring.blade.php
+++ resources/views/presensi/monitoring.blade.php
@@ -156,6 +156,7 @@
             </div>
         </div>
     </div>
+    @role('administrator', 'user')
     <div class="modal modal-blur fade" id="modal-koreksipresensi" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
@@ -170,6 +171,7 @@
             </div>
         </div>
     </div>
+    @endrole
 @endsection
 @push('myscript')
     <script>
